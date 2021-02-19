d2969a362572f5e0787700a28da7369e11530e14
Kevin Tovar <kevintovar@esimez.mx>
2021-02-19 10:21:17 -0600

    Remove bulma, use material ui

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,52 @@
-export default function Header() {
-  return <div>header</div>;
+import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textDecoration: 'none',
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link passHref href="/">
+            <Typography
+              component="a"
+              variant="h6"
+              color="inherit"
+              className={classes.title}
+            >
+              Github Finder
+            </Typography>
+          </Link>
+          <Link href="/users">
+            <Button color="inherit" component="a">
+              Users
+            </Button>
+          </Link>
+          <Link href="/repositories">
+            <Button color="inherit" component="a">
+              Repositories
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,41 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import 'bulma/css/bulma.css';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Header from '../components/Header';
+import theme from '../styles/theme';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Github finder</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
 };
-
-export default MyApp;
